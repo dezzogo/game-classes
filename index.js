7a80ee189d70dd8016112bcfa75d4dc7ec54f1ba
dezzogo <dezzogo@gmail.com>
2025-01-16 18:59:52 -0300

    finishing challenge!

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('just uploading my first commit');
+class hero {
+    constructor (called, age, type) {
+        this.called = called;
+        this.age = age;
+        this.type = type;
+    }
+
+    toAttack() {
+        const heroTypes = ["warrior", "mage", "monk", "ninja"];
+        const heroAttacks = ["sword", "magic", "martial arts", "shouriken"];
+        let attack;
+
+        for (let i=0; i < heroTypes.length; i++) {
+            if (this.type === heroTypes[i]) {
+                attack = heroAttacks[i];
+                break;
+            }
+        }
+
+        console.log(`[EN] Hero ${this.called} (${this.type}) has attacked using ${attack}!`);
+        console.log(`[PT] O heroi ${this.called} (${this.type}) atacou usando ${attack}!`);
+    }
+}
+
+const chandraNalaar = new hero ("Chandra Nalaar", 28, "mage");
+chandraNalaar.toAttack();
+
+const shikamaruNara = new hero ("Shikamaru Nara", 27, "ninja");
+shikamaruNara.toAttack();
